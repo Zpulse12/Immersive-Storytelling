3b5836489cfaf38e3c4544ccf9712718b675580e
Maximilian Duda <s141988@ap.be>
2024-12-16 20:17:47 +0100

    Add stratscene

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("No more scenes to load.");
+        }
+    }
+}
