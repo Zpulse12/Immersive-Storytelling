56ef52254229cddf2027704738d32c1e5a50733e
robbe <s131134@ap.be>
2025-01-02 15:10:17 +0100

    Add timer to loading next scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,18 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
     public int SpotlightsVisited { get; private set; }
-    
+
     private List<ChasePlayer> chasePlayers = new List<ChasePlayer>();
     private bool allStopped = false;
+    private bool isTimerRunning = false;
+    public float sceneTimer = 4;
 
     private void Awake()
     {
@@ -16,7 +20,7 @@ public class GameManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-            Debug.Log("GameManager geïnitialiseerd");
+            Debug.Log("GameManager initialized");
         }
         else
         {
@@ -27,17 +31,15 @@ public class GameManager : MonoBehaviour
     public void IncrementSpotlightsVisited()
     {
         SpotlightsVisited++;
-        Debug.Log($"[GameManager] Spotlight bezocht! Nieuw totaal: {SpotlightsVisited}");
-        
-        Debug.Log($"[GameManager] Trigger positie: {UnityEngine.EventSystems.EventSystem.current?.currentSelectedGameObject?.transform.position}");
+        Debug.Log($"[GameManager] Spotlight visited! New total: {SpotlightsVisited}");
     }
 
     public void ResetSpotlightsVisited()
     {
         SpotlightsVisited = 0;
-        Debug.Log("SpotlightsVisited gereset naar 0");
+        Debug.Log("SpotlightsVisited reset to 0");
     }
-    
+
     public void RegisterChasePlayer(ChasePlayer chasePlayer)
     {
         if (!chasePlayers.Contains(chasePlayer))
@@ -60,15 +62,38 @@ public class GameManager : MonoBehaviour
             }
         }
 
-        if (allStopped)
+        if (allStopped && !isTimerRunning)
         {
-            Debug.Log("All objects have stopped. Loading new scene...");
-            LoadNewScene();
+            Debug.Log("All objects have stopped. Starting timer...");
+            DisableVRMovement(); // Disable VR movement when all have stopped
+            StartCoroutine(AllStoppedCoroutine());
         }
     }
-    
+
+    private IEnumerator AllStoppedCoroutine()
+    {
+        isTimerRunning = true;
+        yield return new WaitForSeconds(sceneTimer);
+
+        Debug.Log("Timer finished. Loading new scene...");
+        LoadNewScene();
+        isTimerRunning = false;
+    }
+
+    private void DisableVRMovement()
+    {
+        // Logic to disable VR movement
+        Debug.Log("VR movement disabled.");
+        
+        var vrController = FindObjectOfType<DynamicMoveProvider>();
+        if (vrController != null)
+        {
+            vrController.enabled = false;
+        }
+    }
+
     public void LoadNewScene()
     {
         SceneManager.LoadScene("Scenes/Endscene");
     }
-} 
+}
